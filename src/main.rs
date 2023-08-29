c694c4501ad43987b11769954cfa18303cd8266c
David Corey <davidmatthewcorey@gmail.com>
2023-08-28 22:16:29 -0600

    Use u8::MAX to calculate the max saturation level of an RGB value.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-const MAX_SATURATION: u8 = 255;
 const PPM_IDENTIFIER: &str = "P3";
 
 fn main() {
@@ -9,26 +8,30 @@ fn main() {
 }
 
 fn render_gradient(width: i32, height: i32) {
-    print_image_headers(width, height, MAX_SATURATION);
-    print_gradient(width, height, MAX_SATURATION)
+    print_image_headers(width, height);
+    print_gradient(width, height);
 }
 
-fn print_image_headers(width: i32, height: i32, saturation: u8) {
+fn print_image_headers(width: i32, height: i32) {
+    let max_saturation = u8::MAX;
+
     println!("{PPM_IDENTIFIER}");
     println!("{width} {height}");
-    println!("{saturation}");
+    println!("{max_saturation}");
 }
 
-fn print_gradient(width: i32, height: i32, max_saturation: u8) {
+fn print_gradient(width: i32, height: i32) {
     for row in 0..height {
         for col in 0..width {
-            let pixel = calc_gradient_pixel(height, width, row, col, max_saturation);
+            let pixel = calc_gradient_pixel(height, width, row, col);
             println!("{} {} {}", pixel.0, pixel.1, pixel.2);
         }
     }
 }
 
-fn calc_gradient_pixel(height: i32, width: i32, row: i32, col: i32, max_saturation: u8) -> (u8, u8, u8) {
+fn calc_gradient_pixel(height: i32, width: i32, row: i32, col: i32) -> (u8, u8, u8) {
+    let max_saturation = u8::MAX;
+    
     let red = col as f32 / (width - 1) as f32;
     let green = row as f32 / (height - 1) as f32;
     let blue = 0;

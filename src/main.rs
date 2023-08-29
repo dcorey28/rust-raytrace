661cf285ffeaca510cc825a95229b5deabac7701
David Corey <davidmatthewcorey@gmail.com>
2023-08-28 22:06:18 -0600

    Switch pixels to be u8 tuples.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-const MAX_SATURATION: i32 = 255;
-const PPM_MAGIC_NUMBER: &str = "P3";
+const MAX_SATURATION: u8 = 255;
+const PPM_IDENTIFIER: &str = "P3";
 
 fn main() {
     let image_width = 256;
@@ -13,13 +13,13 @@ fn render_gradient(width: i32, height: i32) {
     print_gradient(width, height, MAX_SATURATION)
 }
 
-fn print_image_headers(width: i32, height: i32, saturation: i32) {
-    println!("{PPM_MAGIC_NUMBER}");
+fn print_image_headers(width: i32, height: i32, saturation: u8) {
+    println!("{PPM_IDENTIFIER}");
     println!("{width} {height}");
     println!("{saturation}");
 }
 
-fn print_gradient(width: i32, height: i32, max_saturation: i32) {
+fn print_gradient(width: i32, height: i32, max_saturation: u8) {
     for row in 0..height {
         for col in 0..width {
             let pixel = calc_gradient_pixel(height, width, row, col, max_saturation);
@@ -28,14 +28,14 @@ fn print_gradient(width: i32, height: i32, max_saturation: i32) {
     }
 }
 
-fn calc_gradient_pixel(height: i32, width: i32, row: i32, col: i32, max_saturation: i32) -> (i32, i32, i32) {
+fn calc_gradient_pixel(height: i32, width: i32, row: i32, col: i32, max_saturation: u8) -> (u8, u8, u8) {
     let red = col as f32 / (width - 1) as f32;
     let green = row as f32 / (height - 1) as f32;
     let blue = 0;
 
     (
-        (red * max_saturation as f32) as i32,
-        (green * max_saturation as f32) as i32,
+        (red * max_saturation as f32) as u8,
+        (green * max_saturation as f32) as u8,
         blue,
     )
 }
